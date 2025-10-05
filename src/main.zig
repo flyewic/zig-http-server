e66b71262d57668e0a5d10f317460497c3825cfa
flye <{ID}+{username}@users.noreply.github.com>
2025-10-06 00:18:53 +0200

    Lowered thread count for server

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const HttpServer = @import("http_server.zig").HttpServer;
 
 pub fn main() !void {
     const allocator = std.heap.c_allocator;
-    var server = try HttpServer.init("127.0.0.1", 8080, allocator, 8, "./public");
+    var server = try HttpServer.init("127.0.0.1", 8080, allocator, 4, "./public");
     defer server.deinit();
 
     try server.serve();
