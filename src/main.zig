b549d8c2d7028713f8070fd59f9211068ee6b2ad
flye <{ID}+{username}@users.noreply.github.com>
2025-10-05 22:57:27 +0200

    Implemented threading

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const HttpServerModule = @import("http_server.zig");
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-    var server = try HttpServerModule.HttpServer.init("127.0.0.1", 8080, allocator);
+    const allocator = std.heap.c_allocator;
+    var server = try HttpServerModule.HttpServer.init("127.0.0.1", 8080, allocator, 8);
     defer server.deinit();
 
     try server.serve();
